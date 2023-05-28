7afa2359670a654f5237a2a97be28e6a31a37a8e
markvayson <markvayson@gmail.com>
2023-05-28 15:17:09 +0400

    4 fix errors

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -18,7 +18,7 @@ function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
 
 export function NewTweetForm() {
   const session = useSession();
-  if (session.status !== "authenticated") return;
+  if (session.status !== "authenticated") return null;
 
   return <Form />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { type NextPage } from "next";
+import { NextPage } from "next";
 import { NewTweetForm } from "~/components/NewTweetForm";
 
 const Home: NextPage = () => {
