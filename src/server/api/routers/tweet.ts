a25d215c784538bdaab8179433daf10e513617e5
markvayson <markvayson@gmail.com>
2023-05-29 07:56:49 +0400

    add changes

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,5 +1,5 @@
-import { Prisma } from "@prisma/client";
-import { inferAsyncReturnType } from "@trpc/server";
+import type { Prisma } from "@prisma/client";
+import type{ inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
 import {
   createTRPCRouter,
